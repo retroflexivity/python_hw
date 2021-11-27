2fb18da0f695f0f1ff469c062d804993e3be603f
retroflexivity <ark.shaldov@gmail.com>
2021-11-27 23:06:21 +0300

    hw5 something

--- hw5/5.10.py
+++ hw5/5.10.py
@@ -1,12 +1,11 @@
-bookname = '4300-0.txt'
+bookname = 'pg66655.txt'
 book = open(bookname)
 words = pars = 0
 
 for line in book:
-    # print(line)
     if line == '\n':
         pars += 1
     else:
         words += line[:-1].count(' ') + 1
 
-print(pars, words, words / pars)
+print(words / pars)
--- hw5/5.6.py
+++ hw5/5.6.py
@@ -2,6 +2,6 @@ bookname = '4300-0.txt'
 booklines = open(bookname).readlines()
 
 print('# https://gutenberg.org/files/4300/4300-0.txt')
-#так, что ли? странное задание
 print(bookname)
+#так, что ли? странное задание
 print(len(booklines))
