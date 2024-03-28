04f1baeff9bf08e57cce63edd4b4bbebe7d83cb0
retroflexivity <ark.shaldov@gmail.com>
2024-03-28 16:49:08 +0100

    sth

--- pt1/7/5.py
+++ pt1/7/5.py
@@ -2,4 +2,4 @@ def jumble(word):
     from random import sample
     return word[0] + ''.join(sample(word[1:-1], len(word) - 2)) + word[-1]
 
-jumble('ретрофлексивность')
+print(jumble('ретрофлексивность'))
